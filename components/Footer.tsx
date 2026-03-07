2530ff2968ce08467152a63e37780c5a28c4fef5
Curtis Chen <curtischen1234@gmail.com>
2026-03-06 22:17:19 -0800

    change footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer p-4 bg-primary fixed-bottom">
       <div className="container d-flex justify-content-center">
-        <span className="text-light">© 2026 AskAI</span>
+        <span className="text-light">© 2026 QuizAI</span>
       </div>
     </footer>
   );
